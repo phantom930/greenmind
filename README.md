8fb97b0ffd788876d500a756733f48a4807e6f2f
Leonardo Muniz <lmuniz@odoogap.com>
2021-12-22 15:00:50 -0300

    [IMP] bootstrap boilerplate vsf odoo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Greenmind
 
 https://www.bluecity.dk + https://greenmind.dk
+
+## How to start?
+
+```sh
+1. git clone https://git.odoogap.com/customer-projects/greenmind 
+2. yarn install
+3. yarn build # (optional) Verify if everything works properly by building all three projects
+4. yarn dev
+```
---
+++ middleware.config.js
@@ -0,0 +1,15 @@
+const odooBaseUrl = process.env.BASE_URL;
+const graphqlBaseUrl = `${odooBaseUrl}graphql/vsf`;
+
+module.exports = {
+  integrations: {
+    odoo: {
+      location: '@vue-storefront/odoo-api/server',
+      configuration: {
+        odooBaseUrl,
+        graphqlBaseUrl
+      }
+    }
+  }
+};
+
