e171f49ef761ddafc2e99c09b5ebf034cc424c12
ar <aramos@odoogap.com>
2022-01-31 14:57:14 +0000

    [IMP] added font/color variants and breadcrumbs configuration

--- pages/Home.vue
+++ pages/Home.vue
@@ -11,12 +11,16 @@
     :price="price"
     :currency="currency"
     /> -->
-    <GreenBannerVertical
+    <!-- <GreenBannerVertical
     banner_title="Stand"
     banner_description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands."
     banner_button="SE MERE"
     banner_image="banner_1"
+    /> -->
+    <SfBreadcrumbs 
+    :breadcrumbs= breadcrumb
     />
+    <SfButton />
     <br>
     <br>
     <br>
@@ -24,15 +28,43 @@
 </template>
 
 <script>
+import { SfBreadcrumbs, SfButton } from '@storefront-ui/vue'
 export default {
+  components: {
+    SfBreadcrumbs,
+    SfButton
+  },
   data() {
     return {
         item: ["iPhone8", "iPhone13", "iPhone8", "iPhone8", "iPhone8"],
         storage: ["128 Gb","128 Gb","128 Gb","128 Gb","128 Gb"],
         color: ["Gold", "Red", "Silver", "Black", "Gold"],
         price: ["2,999", "2,999", "2,999", "2,999", "2,999",],
-        currency: ["$"]
+        currency: ["$"],
+        breadcrumb: [
+        {
+          "text": "Home",
+          "link": "#"
+        },
+        {
+          "text": "Category",
+          "link": "#"
+        },
+        {
+          "text": "Pants",
+          "link": "#"
+        }
+      ]
     }
   }
 }
 </script>
+
+<style lang="scss">
+:root {
+  --font-family--primary: "Josefin Sans";
+  @include generate-color-variants(--_c-green-primary, #7BA393);
+  @include assign-color-variants(--c-primary, --_c-green-primary);
+  @include assign-color-variants(--c-secondary, --_c-green-secondary); 
+}
+</style>
