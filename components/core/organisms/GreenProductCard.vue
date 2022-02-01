95f555bea928d2a52551537780dd65e13b37f6ce
ar <aramos@odoogap.com>
2022-02-01 17:30:34 +0000

    [IMP] added product card component

---
+++ components/core/organisms/GreenProductCard.vue
@@ -0,0 +1,68 @@
+<template>
+    <SfProductCard
+    :image="require('/assets/images/product/'+product_card_image+'.svg')"
+    :imageWidth="157"
+    :imageHeight="307"
+    :alt="product_card_image"
+    :showAddToCartButton="false"
+    :title= product_card_title
+    scoreRating = "false"
+    wishlistIcon = "false"
+    :regularPrice = product_card_regularPrice
+    :specialPrice = product_card_specialPrice
+    :link="'/' + product_card_link"
+    />
+</template>
+
+<script>
+import { SfProductCard } from '@storefront-ui/vue'
+export default {
+    components: {
+        SfProductCard
+    },
+    props: {
+        product_card_title: {
+            type: String,
+            default: ''
+        },
+        product_card_image: {
+            type: String,
+            default: ''
+        },
+        product_card_regularPrice: {
+            type: String,
+            default: ''
+        },
+        product_card_specialPrice: {
+            type: String,
+            default: ''
+        },
+        product_card_link: {
+            type: String,
+            default: '#'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .sf-image {
+    transform: translate(35%);
+}
+::v-deep .sf-product-card__title {
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 26px;
+    font-weight: 500;
+    text-align: center;
+}
+::v-deep .sf-price__regular{
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 26px;
+    font-weight: 500;
+    margin-left: auto;
+    margin-right: auto;
+}
+::v-deep .sf-product-card {
+    border-radius: 20px;
+}
+</style>
---
+++ pages/Test.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <h2>Comming soon</h2>
+    <!-- <GreenFooter /> -->
+    <!-- <GreenEmailForm /> -->
+    <!-- <GreenPrimaryButton :buttonPlaceholder="$t('SE FLERE')"/> -->
+    <!-- <GreenCarousel
+    :item="item"
+    :feature1="storage"
+    :feature2="color"
+    :price="price"
+    :currency="currency"
+    /> -->
+    <!-- <GreenBannerVertical
+    banner_title="Stand"
+    banner_description="Find stunning women's cocktail dresses and party dresses. Stand out in lace and metallic cocktail dresses and party dresses from all your favorite brands."
+    banner_button="SE MERE"
+    banner_image="banner_1"
+    /> -->
+    <!-- <SfBreadcrumbs 
+    :breadcrumbs= breadcrumb
+    /> -->
+    <GreenProductCard
+    product_card_title="iPhone 8"
+    product_card_regularPrice=" Fra 2.999,-"
+    product_card_image="iPhone8"
+    />
+    <br>
+    <br>
+    <br>
+  </div>
+</template>
+
+<script>
+import { SfBreadcrumbs, SfButton } from '@storefront-ui/vue'
+export default {
+  components: {
+    SfBreadcrumbs,
+    SfButton
+  },
+  data() {
+    return {
+        item: ["iPhone8", "iPhone13", "iPhone8", "iPhone8", "iPhone8"],
+        storage: ["128 Gb","128 Gb","128 Gb","128 Gb","128 Gb"],
+        color: ["Gold", "Red", "Silver", "Black", "Gold"],
+        price: ["2,999", "2,999", "2,999", "2,999", "2,999",],
+        currency: ["$"],
+        breadcrumb: [
+        {
+          "text": "Home",
+          "link": "#"
+        },
+        {
+          "text": "Category",
+          "link": "#"
+        },
+        {
+          "text": "Pants",
+          "link": "#"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+:root {
+  --font-family--primary: "Josefin Sans";
+  @include generate-color-variants(--_c-green-primary, #7BA393);
+  @include assign-color-variants(--c-primary, --_c-green-primary);
+  @include assign-color-variants(--c-secondary, --_c-green-secondary); 
+}
+</style>
