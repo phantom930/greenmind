d86e2f60a69feefbc7a5f0456edce343b678ea5a
ar <aramos@odoogap.com>
2022-02-07 15:57:46 +0000

    [IMP] added hero component to home

---
+++ components/core/organisms/GreenHero.vue
@@ -0,0 +1,38 @@
+<template>
+  <SfHero>
+    <SfHeroItem
+    title="Colorful summer dresses are already in store"
+    buttonText="SE MERE"
+    background="#ECEFF1"
+    :image="require('/assets/images/banner/'+hero_image+'.svg')"
+    />
+    <SfHeroItem
+    title="Colorful summer dresses are already in store"
+    buttonText="SE MERE"
+    background="#ECEFF1"
+    image="/assets/storybook/SfHero/hero.png"
+    />
+    <SfHeroItem
+    title="Colorful summer dresses are already in store"
+    buttonText="SE MERE"
+    background="#ECEFF1"
+    image="/assets/storybook/SfHero/hero.png"
+    />
+  </SfHero>
+    
+</template>
+
+<script>
+import { SfHero } from '@storefront-ui/vue'
+export default {
+    components: {
+        SfHero
+    },
+    props: {
+        hero_image: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
--- pages/Home.vue
+++ pages/Home.vue
@@ -1,9 +1,31 @@
 <template>
   <div>
-    <h2>Comming soon</h2>
     <!-- <GreenFooter /> -->
     <!-- <GreenEmailForm /> -->
     <!-- <GreenPrimaryButton :buttonPlaceholder="$t('SE FLERE')"/> -->
+    <SfHero>
+      <SfHeroItem
+      title="Colorful summer dresses are already in store"
+      buttonText="SE MERE"
+      background="none"
+      :image="require('/assets/images/homeHero/homeHero_1.svg')"
+      style="background-size: auto; background-position-y: center;"
+      />
+      <SfHeroItem
+      title="Colorful summer dresses are already in store"
+      buttonText="SE MERE"
+      background="none"
+      :image="require('/assets/images/homeHero/homeHero_1.svg')"
+      style="background-size: auto; background-position-y: center;"
+      />
+      <SfHeroItem
+      title="Colorful summer dresses are already in store"
+      buttonText="SE MERE"
+      background="none"
+      :image="require('/assets/images/homeHero/homeHero_1.svg')"
+      style="background-size: auto; background-position-y: center;"
+      />
+    </SfHero>
     <div style="display: flex">
       <GreenBannerVertical
       banner_title="Stand"
@@ -45,11 +67,12 @@
 </template>
 
 <script>
-import { SfBreadcrumbs, SfButton } from '@storefront-ui/vue'
+import { SfBreadcrumbs, SfButton, SfHero } from '@storefront-ui/vue'
 export default {
   components: {
     SfBreadcrumbs,
-    SfButton
+    SfButton,
+    SfHero
   },
   data() {
     return {
@@ -83,5 +106,56 @@ export default {
   @include generate-color-variants(--_c-green-primary, #7BA393);
   @include assign-color-variants(--c-primary, --_c-green-primary);
   @include assign-color-variants(--c-secondary, --_c-green-secondary); 
+}
+</style>
+
+<style lang="scss" scoped>
+::v-deep .sf-hero {
+  height: 480px;
+  margin-top: -75px;
+}
+::v-deep .sf-hero-item__title{
+  font: var(--font-family--primary);
+  color: #FFFFFF;
+  font-size: 46px;
+  font-weight: 700;
+  line-height: 48px;
+  width: 125%;
+  margin-top: 0;
+}
+::v-deep .sf-hero-item__button .sf-button {
+    border-radius: 40px;
+    --button-background: #32463D;
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 14px;
+    font-weight: 500;
+    text-decoration: none;
+    padding: 18px 52px;
+    transform: translate(0%, -50%);
+}
+::v-deep .sf-hero-item__button .sf-button:hover{
+    --button-background: #78A886;
+    --button-box-shadow-opacity: 0.25;
+    --button-background: #78A886 radial-gradient(circle, transparent 1%, #32463D 1%) center/15000%;
+}
+::v-deep .sf-hero-item__button .sf-button:active {
+    --button-box-shadow: none;
+    --button-background: #32463D radial-gradient(circle, transparent 40%, #78A886 1%) center/15000%;
+    --button-transition: background 0s ;
+    --button-text-decoration: underline;
+  background-size: 100%;
+}
+::v-deep .sf-hero__bullets {
+  transform: translate3d(-50%, -55px, 0);
+  --bullet-background: #A5CEBE;
+}
+::v-deep .sf-bullet.is-active {
+  --bullet-background: #32463D;
+}
+::v-deep .sf-hero__control--left {
+  display: none;
+}
+::v-deep .sf-hero__control--right {
+  display: none;
 }
 </style>
