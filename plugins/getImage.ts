9c9c6f912456599f8a170554d27f657144c61e25
Leonardo Muniz <lmuniz@odoogap.com>
2022-02-15 16:22:10 -0300

    [IMP] returning correct image way

--- plugins/getImage.ts
+++ plugins/getImage.ts
@@ -1,8 +1,6 @@
 export default ({ app }, inject) => {
     const getImage =  (imagePath: string) => {
-        //@TODO temporary removing base url until api response was OK
-        // return `${app.$config.baseURL}${imagePath?.replace('/', '')}`; 
-        return imagePath; 
+         return `${app.$config.baseURL}${imagePath?.replace('/', '')}`; 
     }
     inject('image', getImage)
 }
