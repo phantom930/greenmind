0a185515e89ef504fd45274ce2b1e6bdc27fbf32
OgnjenMatijevic <matijevicognjen1@gmail.com>
2022-02-16 14:42:11 +0100

    [ADD] sf mega menu

--- components/core/organisms/GreenTopbarTertiary.vue
+++ components/core/organisms/GreenTopbarTertiary.vue
@@ -1,5 +1,6 @@
 <template>
-  <SfHeader>
+  <div class="dropdown-wrap">
+    <SfHeader>
       <template #navigation>
         <div class="header-links">
           <SfHeaderNavigationItem
@@ -11,15 +12,130 @@
             :link="localePath(`/c/${category.slug}/${category.id}`)"
           />
         </div>
+  <div class="dropdown">
+    <SfMegaMenu title="Man" visible>
+    <SfMegaMenuColumn title="Samsung">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn title="Huawei">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn title="Sony">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn title="One Plus">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn title="Category">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn title="Category">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <SfMegaMenuColumn title="Category">
+      <SfList>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+        <a href="#"><SfListItem><SfMenuItem label="Skirts" />Boots</SfListItem></a>
+      </SfList>
+    </SfMegaMenuColumn>
+    <!-- <SfMegaMenuColumn
+      class="
+        sf-mega-menu-column--pined-content-on-mobile
+        sf-mega-menu-column--hide-header-on-mobile
+      "
+      title="Featured"
+    >
+      <div class="smartphone-only">
+        <SfBanner
+          title="THE OFFICE LIFE"
+          subtitle="T-shirts"
+          :image='{"mobile":"/assets/storybook/SfMegaMenu/bannerSandals.jpg","desktop":"/assets/storybook/SfMegaMenu/bannerSandals.jpg"}'
+        />
+        <SfBanner
+          title="ECO SANDALS"
+          subtitle="T-shirts"
+          :image='{"mobile":"/assets/storybook/SfMegaMenu/bannerBeachBag.jpg","desktop":"/assets/storybook/SfMegaMenu/bannerBeachBag.jpg"}'
+        />
+      </div>
+      <div class="desktop-only">
+        <SfBanner
+          title="THE OFFICE LIFE"
+          subtitle="T-shirts"
+          :image='{"mobile":"/assets/storybook/SfMegaMenu/bannerSandals.jpg","desktop":"/assets/storybook/SfMegaMenu/bannerSandals.jpg"}'
+        />
+        <SfBanner
+          title="ECO SANDALS"
+          subtitle="T-shirts"
+          :image='{"mobile":"/assets/storybook/SfMegaMenu/bannerBeachBag.jpg","desktop":"/assets/storybook/SfMegaMenu/bannerBeachBag.jpg"}'
+        />
+      </div>
+    </SfMegaMenuColumn> -->
+  </SfMegaMenu>
+  </div>
       </template>
   </SfHeader>
+  </div>
 </template>
 
 <script>
-import { SfHeader } from '@storefront-ui/vue';
+import { SfHeader, SfMegaMenu } from '@storefront-ui/vue';
 export default {
   components: {
-    SfHeader
+    SfHeader,
+    SfMegaMenu
   },
   data() {
       return {
@@ -65,4 +181,67 @@ export default {
   font-size: 14px;
   color: #1D1F22;
 }
+
+::v-deep .dropdown {
+  /* display: none; */
+  position: absolute;
+  top: 80px;
+  left: 0;
+  width: 100%;
+  background: #fff;
+}
+
+::v-deep .sf-header:hover .dropdown {
+  display: block;
+}
+
+.dropdown-wrap {
+  position: relative;
+}
+
+::v-deep .sf-mega-menu__content {
+  max-width: 1208px;
+  width: 100%;
+  padding-bottom: 80px;
+}
+
+::v-deep .sf-menu-item__label {
+  font-size: 18px;
+  font-weight: 400;
+  color: #1D1F22;
+  font-family: "Josefin Sans";
+  text-transform: none;
+  transition: all 0.3s;
+}
+
+::v-deep .sf-list__item::marker {
+  content: none;
+}
+
+::v-deep .sf-list__item {
+  color: #43464E;
+  margin-bottom: 15px;
+  border-bottom: 1px solid transparent;
+}
+
+::v-deep .sf-list__item:hover {
+  border-bottom: 1px solid #7BA393;
+  color: #7BA393;
+}
+
+::v-deep .sf-mega-menu-column {
+  max-width: 100px;
+  width: 100%;
+}
+
+::v-deep .sf-mega-menu-column .sf-mega-menu-column__content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+::v-deep .sf-mega-menu-column:hover .sf-menu-item__label {
+  color: #7BA393;
+}
+
 </style>
