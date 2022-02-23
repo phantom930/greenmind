65041d248fe980b05e4180cdc099761fb0d985dc
Afonso Ramos <aramos@odoogap.com>
2022-02-23 11:11:29 +0000

    [FIX] fixed custom querie dependency error

--- middleware.config.js
+++ middleware.config.js
@@ -1,5 +1,6 @@
 const odooBaseUrl = process.env.BACKEND_BASE_URL || process.env.BASE_URL || 'https://vsfdemo.labs.odoogap.com/';
 const graphqlBaseUrl = `${odooBaseUrl}graphql/vsf`;
+const customQueries = require('./geven-api/customQueries');
 
 module.exports = {
   integrations: {
