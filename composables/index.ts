ed1ee2121baea2a5a375cc204716bbaa30e388c6
Leonardo Muniz <lmuniz@odoogap.com>
2022-03-02 08:25:25 -0300

    [ADD] state ui composable

--- composables/index.ts
+++ composables/index.ts
@@ -3,11 +3,13 @@ import useUiNotification from './useUiNotification';
 import useUiCategoryHelpers from './useUiCategoryHelpers';
 import useUiState from './useUiState';
 import useCurrency from './useCurrency';
+import useUiStyleState from './useUiStyleState';
 
 export {
   useUiHelpers,
   useUiNotification,
   useUiCategoryHelpers,
   useUiState,
-  useCurrency
+  useCurrency,
+  useUiStyleState
 };
