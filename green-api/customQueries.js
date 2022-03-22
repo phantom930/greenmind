4a8d472530e290a9bc17fcf30dceb7bdce905db5
Leonardo Muniz <lmuniz@odoogap.com>
2022-03-22 18:23:08 -0300

    [IMP] custom sheeper category query

--- green-api/customQueries.js
+++ green-api/customQueries.js
@@ -209,5 +209,33 @@ module.exports = {
         }
       }
       `
+  }),
+  greenGetCategories: ({variables}) => ({
+    variables,
+    query: gql `query(
+      $search: String
+      $filter: CategoryFilterInput
+      $currentPage: Int
+      $pageSize: Int
+      $sort: CategorySortInput
+    ) {
+      categories(
+        search: $search
+        filter: $filter
+        currentPage: $currentPage
+        pageSize: $pageSize
+        sort: $sort
+      ) {
+        categories {
+          id
+          name
+          slug
+          parent {
+            id
+            name
+          }
+        }
+      }
+    }`
   })
 };
