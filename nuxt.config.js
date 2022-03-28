df9dff5fb2f964299d59812b7d75069e7cb42390
Leonardo Muniz <lmuniz@odoogap.com>
2022-03-28 16:28:25 -0300

    [IMP] fix wrong import of font family / using nuxt import module

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,19 +49,6 @@ export default {
         rel: 'preconnect',
         href: 'https://fonts.gstatic.com',
         crossorigin: 'crossorigin'
-      },
-      {
-        rel: 'preload',
-        href:
-          'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600;700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap',
-        as: 'style'
-      },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;600;700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap',
-        media: 'print',
-        onload: 'this.media=\'all\''
       }
     ]
   },
@@ -79,6 +66,14 @@ export default {
       useWebmanifestExtension: false
     }
   },
+  googleFonts: {
+    families: {
+      'Josefin+Sans': true,
+      wght: [300, 400, 500, 600, 700],
+      Lato: [100, 300]
+    },
+    display: 'swap'
+  },
   loading: { color: '#fff' },
   plugins: ['~/plugins/getImage.ts'],
   device: {
@@ -93,6 +88,7 @@ export default {
     '@nuxtjs/tailwindcss',
     '@nuxt/typescript-build',
     '@vueuse/nuxt',
+    ['@nuxtjs/google-fonts'],
     '@nuxtjs/style-resources',
     [
       '@vue-storefront/nuxt',
