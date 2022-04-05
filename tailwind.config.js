604bfea90e6636bb85a51faefe27f82ead04c265
Leonardo Muniz <lmuniz@odoogap.com>
2022-04-05 18:03:00 -0300

    [FIX] purge css not run in dev mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+const isDev = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: !isDev,
     content: [
       'components/**/*.vue',
       'layouts/**/*.vue',
