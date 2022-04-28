007192ed775335227e21e770623be544c13dad89
Leonardo Muniz <lmuniz@odoogap.com>
2022-04-27 23:06:57 -0300

    [FIX] digit validation

--- plugins/vee-validate.ts
+++ plugins/vee-validate.ts
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import { extend } from 'vee-validate';
-import { min, email, required } from 'vee-validate/dist/rules';
+import { min, email, required, digits } from 'vee-validate/dist/rules';
 extend('min', {
   ...min,
   message: 'This field must have be greater than 0'
@@ -9,6 +9,11 @@ extend('min', {
 extend('email', {
   ...email,
   message: 'This field must be an email'
+});
+
+extend('digits', {
+  ...digits,
+  message: 'This field must have all digits'
 });
 
 extend('required', {
