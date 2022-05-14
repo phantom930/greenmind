e4cffa16f1dfdd208234608e134bb3a75388e455
Leonardo Muniz <lmuniz@odoogap.com>
2022-05-14 17:52:34 -0300

    [IMP] update shops page

--- pages/HowToSell.vue
+++ pages/HowToSell.vue
@@ -1,55 +1,61 @@
 <template>
-  
   <div class="text-wrap">
     <div class="container">
-      <h1 class="font-bold title">Sådan sælger du</h1>
-      <table>
+      <h1 class="font-bold title">
+        Sådan sælger du
+      </h1>
+      <table class="sell-table">
         <tr>
-          <td></td>
-          <td></td>
+          <td />
+          <td />
         </tr>
         <tr>
-          <td></td>
-          <td>Prisgaranti
+          <td />
+          <td>
+            Prisgaranti
             Vi slår alle andre firmaers opkøbspriser på nettet.Butikkerne vurderer prisen på dit produkt baseret på standen.
           </td>
         </tr>
         <tr>
-          <td></td>
-          <td></td>
+          <td />
+          <td />
         </tr>
         <tr>
-          <td></td>
-          <td class="text">Nemt og smart
+          <td />
+          <td class="text">
+            Nemt og smart
             Vi renser, nulstiller og sletter dine data, så e-mails, private billeder og videoer ikke ender i de forkerte hænder. Du handler udelukkende med GreenMind og vi behandler alle dine oplysninger fortroligt.
           </td>
         </tr>
       </table>
 
-      <p class="text">Inden du kommer ned i butikken</p>
+      <p class="text">
+        Inden du kommer ned i butikken
+      </p>
       <table>
         <tr>
-          <td></td>
+          <td />
         </tr>
         <tr>
           <td class="text text-center">
             Produktet skal være opladet
-            Hvis det er en mobil, tablet, bærbar eller lignende der bruger batteri, skal du sørge for at produktet er opladet, 
+            Hvis det er en mobil, tablet, bærbar eller lignende der bruger batteri, skal du sørge for at produktet er opladet,
             så personalet kan tjekke udstyrets stand.
           </td>
         </tr>
         <tr>
-          <td></td>
+          <td />
         </tr>
         <tr>
-          <td class="text text-center">Medbring ID
+          <td class="text text-center">
+            Medbring ID
             Medbring gyldig billede- og adresselegitimation.
             Alle som sælger til GreenMind bliver registreret i vores system for at sikre at du er den retmæssige ejer.
             Du kan læse mere om betingelserne <a href="#" class="underline y-link">her.</a>
           </td>
         </tr>
         <tr>
-          <td></td>
+          <td />
         </tr>
         <tr>
           <td class="text text-center">
@@ -59,49 +65,75 @@
         </tr>
       </table>
 
-      <p class="text">Online</p>
+      <p class="text">
+        Online
+      </p>
       <ul class="list-disc">
-        <li class="text">Find din enhed på hjemmesiden, besvar et par spørgsmål og få en pris</li>
-        <li class="text">Ønsker du at købe et andet produkt i vores butik eller onlineshop, kan vi give dig en endnu bedre pris for dit produkt.</li>
-        <li class="text">Det er gratis - vi sender en pakkelabel til dig på mail.</li>
-        <li class="text">Få hurtig betaling. Vi undersøger din enhed og sender pengene 2 dage efter modtagelse </li>
-        <li class="text">Herefter kan du enten bruge beløbet i butikken, onlineshoppen eller få pengene overført til din bankkonto.
+        <li class="text">
+          Find din enhed på hjemmesiden, besvar et par spørgsmål og få en pris
+        </li>
+        <li class="text">
+          Ønsker du at købe et andet produkt i vores butik eller onlineshop, kan vi give dig en endnu bedre pris for dit produkt.
+        </li>
+        <li class="text">
+          Det er gratis - vi sender en pakkelabel til dig på mail.
+        </li>
+        <li class="text">
+          Få hurtig betaling. Vi undersøger din enhed og sender pengene 2 dage efter modtagelse
+        </li>
+        <li class="text">
+          Herefter kan du enten bruge beløbet i butikken, onlineshoppen eller få pengene overført til din bankkonto.
           Ved bankoverførsel går der normalt 2 bankdage før pengene indgår på din konto. I sjældne tilfælde kan der dog gå op til 4 bankdage.
         </li>
       </ul>
 
       <a href="#" class="underline text-center y-link subtitle">Se mere her</a>
 
-      <h3 class="font-bold subtitle text-center">I butikken</h3>
-      <p class="text">Personalet vurderer produktet og giver dig en pris
+      <h3 class="font-bold subtitle text-center">
+        I butikken
+      </h3>
+      <p class="text">
+        Personalet vurderer produktet og giver dig en pris
       </p>
       <ul class="list-disc">
-        <li class="text">Jo bedre stand produktet er i, jo bedre pris får du.</li>
-        <li class="text">Ønsker du at købe et andet produkt i vores butik eller onlineshop, kan vi give dig en endnu bedre pris for dit produkt.</li>
-        <li class="text">Når du accepterer prisen, er handlen bindende for begge parter. Vi forbeholder os dog retten til at ophæve handlen såfremt det viser sig, at varen ikke tilhører dig, eller hvis varen har en ikke uvæsentlig defekt eller skade, som du ikke oplyste om forud for salget.</li>
-        <li class="text">Bemærk at personalet til enhver tid kan afvise at købe en vare.</li>
-        <li class="text">Herefter kan du enten bruge beløbet i butikken, onlineshoppen eller få pengene overført til din bankkonto.
+        <li class="text">
+          Jo bedre stand produktet er i, jo bedre pris får du.
+        </li>
+        <li class="text">
+          Ønsker du at købe et andet produkt i vores butik eller onlineshop, kan vi give dig en endnu bedre pris for dit produkt.
+        </li>
+        <li class="text">
+          Når du accepterer prisen, er handlen bindende for begge parter. Vi forbeholder os dog retten til at ophæve handlen såfremt det viser sig, at varen ikke tilhører dig, eller hvis varen har en ikke uvæsentlig defekt eller skade, som du ikke oplyste om forud for salget.
+        </li>
+        <li class="text">
+          Bemærk at personalet til enhver tid kan afvise at købe en vare.
+        </li>
+        <li class="text">
+          Herefter kan du enten bruge beløbet i butikken, onlineshoppen eller få pengene overført til din bankkonto.
           Ved bankoverførsel går der normalt 1-2 bankdage før pengene indgår på din konto. I sjældne tilfælde kan der dog gå op til 4 bankdage.
         </li>
       </ul>
       <a href="/shops" class="underline text-center y-link">Find butikker</a>
 
-      <h3 class="font-bold subtitle text-center">Virksomheder</h3>
-      <p class="text italic">Er I en virksomhed med mere end 10 enheder i vil sælge? <a href="#" class="underline y-link">Læs mere her.</a></p>
+      <h3 class="font-bold subtitle text-center">
+        Virksomheder
+      </h3>
+      <p class="text italic">
+        Er I en virksomhed med mere end 10 enheder i vil sælge? <a href="#" class="underline y-link">Læs mere her.</a>
+      </p>
       <ul class="list-disc">
         <li class="text">
-          Ved at sælge til GreenMind, sørger vi for at jeres virksomhed er i compliance med GDPR-lovgivningen, 
+          Ved at sælge til GreenMind, sørger vi for at jeres virksomhed er i compliance med GDPR-lovgivningen,
           da certificeret datasletning, altid er indeholdt i vores priser.
         </li>
         <li class="text">
-          Såfremt I vælger at lade jeres medarbejdere beholde udgåede enheder, er det stadig vigtigt, 
-          at få dokumentation/certifikat på at jeres enheder er dataslettet, så i til enhver tid kan dokumentere 
+          Såfremt I vælger at lade jeres medarbejdere beholde udgåede enheder, er det stadig vigtigt,
+          at få dokumentation/certifikat på at jeres enheder er dataslettet, så i til enhver tid kan dokumentere
           at i har behandlet fortrolige/personfølsomme oplysninger efter lovgivningen.
         </li>
       </ul>
     </div>
   </div>
-
 </template>
 
 <style lang="scss" scoped>
