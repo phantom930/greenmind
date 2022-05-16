5de4e835d9d19110f7d2600cf789ec083e18d830
Leonardo Muniz <lmuniz@odoogap.com>
2022-05-16 17:06:26 -0300

    [IMP] update customer service page

--- pages/CustomerService.vue
+++ pages/CustomerService.vue
@@ -1,24 +1,33 @@
 <template>
-  
   <div class="text-wrap">
     <div class="container">
-      <h1 class="font-bold title">Kundeservice</h1>
+      <h1 class="font-bold title">
+        Kundeservice
+      </h1>
       <p class="text">
-        Hvis du bliver forvirret eller usikker på, om du vil sælge din brugte mobil telefon, er du velkommen til at kontakte vores kundeservice. 
-        De vil besvare alle dine spørgsmål og guide dig, hvis du har brug for det.
+        Hvis du er usikker på, om du vil sælge din brugte mobil telefon, eller hvis du har spørgsmål før du køber, så er du velkommen til at kontakte vores kundeservice. De vil besvare alle dine spørgsmål og guide dig, hvis du har brug for det.
       </p>
-      <p class="text">Kontakt os i dag</p>
-      <p class="text">Vores kunderservice har åbent i dagene:</p>
-      <p class="text">Mandag til fredag <span class="bold">10 - 17:00</span></p>
       <p class="text">
-        Så har du spørgsmål, ris eller ros, er du altid velkommen til at kontakte os på telefon eller mail, 
+        Vores kundeservice har åbent i dagene:
+      </p>
+      <p class="text">
+        Mandag til fredag kl. <span class="bold">10:00 - 16:00</span>
+      </p>
+      <p class="text">
+        Så har du spørgsmål, ris eller ros, er du altid velkommen til at kontakte os på telefon eller mail,
         så vender vi tilbage til dig hurtigst muligt.
       </p>
-      <p class="text">Ring til os</p>
-      <p class="text">Send en mail</p>
+      <p class="text">
+        Ring til os: <span class="font-bold">+45 4697 5050</span>
+      </p>
+      <p class="text">
+        Send en mail:
+        <a href="mailto: mail@greenmind.dk">
+          mail@greenmind.dk
+        </a>
+      </p>
     </div>
   </div>
-  
 </template>
 
 <style lang="scss" scoped>
