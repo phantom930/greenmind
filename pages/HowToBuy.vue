9452a70aaaeee85ae6e164d572fe9b402959b557
Leonardo Muniz <lmuniz@odoogap.com>
2022-05-16 16:50:31 -0300

    [IMP] update how to buy page

--- pages/HowToBuy.vue
+++ pages/HowToBuy.vue
@@ -1,35 +1,49 @@
 <template>
-  
   <div class="text-wrap">
     <div class="container">
-      <h1 class="font-bold title">Sådan køber du</h1>
-      <p class="text">
+      <h1 class="font-bold title">
+        Sådan køber du
+      </h1>
+      <p class="font-bold">
         Køb brugt med god samvittighed
+      </p>
+      <p class="text">
         Er du en af dem, der er lidt ligeglad med, om det er sidste års model – bare du får god kvalitet?
       </p>
       <p class="text">
-        Køb din iPhone, Samsung Galaxy, Nokia Lumia eller iPad hos GreenMind. Vi køber bl.a. 
+        Køb din iPhone, Samsung Galaxy, Nokia Lumia eller iPad hos GreenMind. Vi køber bl.a.
         brugt elektronik af dem, der altid skal have det nyeste, så du kan købe brugt med god samvittighed.
       </p>
-      <p class="text italic">Nemt og smart</p>
+      <p class="font-bold">
+        Nemt og smart
+      </p>
       <p class="text">
-        Der er tusindvis af kroner at spare på brugt kvalitetselektronik hos GreenMind. 
-        Ofte er den primære forskel på sidste års model og den nyeste designmæssige fordele. 
+        Der er tusindvis af kroner at spare på brugt kvalitetselektronik hos GreenMind.
+        Ofte er den primære forskel på sidste års model og den nyeste designmæssige fordele.
         Vi har desuden en række procedurer, der sikrer, at du får en vare, der fungerer og er renset for den tidligere ejers data.
       </p>
       <ul class="list-disc">
-        <li class="text">Alle brugte produkter bliver renset, nulstillet og klargjort for at sikre, at du får en vare, der er uden fejl og mangler.</li>
-        <li class="text">Du får 2 års garanti på brugt elektronik købt hos GreenMind, gældende fra købstidspunktet.</li>
         <li class="text">
-          Alle, som sælger til GreenMind, bliver registreret i vores system og skal vise gyldig billed- og adresselegitimation for at sikre, 
+          Alle brugte produkter bliver renset, nulstillet og klargjort for at sikre, at du får en vare, der er uden fejl og mangler.
+        </li>
+        <li class="text">
+          Du får 2 års garanti på brugt elektronik købt hos GreenMind, gældende fra købstidspunktet.
+        </li>
+        <li class="text">
+          Alle, som sælger til GreenMind, bliver registreret i vores system og skal vise gyldig billed- og adresselegitimation for at sikre,
           at de er den retmæssige ejer.
         </li>
       </ul>
-      <p class="text italic">Find GreenMind</p>
-      <p class="text">Klik <a href="#" class="underline y-link">her</a> og se, hvor du kan købe brugt elektronik.</p>
+      <p class="font-bold">
+        Find GreenMind
+      </p>
+      <p class="text">
+        Klik <nuxt-link :to="{ name: 'shops' }" class="underline y-link">
+          her
+        </nuxt-link> og se, hvor du kan købe brugt elektronik.
+      </p>
     </div>
   </div>
-
 </template>
 
 <style lang="scss" scoped>
