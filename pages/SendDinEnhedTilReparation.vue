be5a870fc16990d564a009d63f3ec20877e8335e
Jesper Witt <jw@bluecity.dk>
2022-05-20 13:14:56 +0000

    Update pages/SendDinEnhedTilReparation.vue

---
+++ pages/SendDinEnhedTilReparation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="text-wrap">
+    <div class="container">
+      <h1 class="font-bold title mb">
+        Reparation
+      </h1>
+      <p class="text mb">
+        Find en butik nær dig og få din telefon repareret i dag. Hos GreenMind ønsker vi at være de førende inden for både reparationer og salg af brugte smartphones, tablets og computere. For at være det, er det afgørende, at vi kan være så tæt på vores kunder som overhovedet muligt. Derfor har vi i dag butikker fordelt over hele landet. Som kunde skal du som regel ikke vente længe på, at vi reparerer din enhed. Mens du venter, sørger vi for en behagelig atmosfære i vores butikker, hvor du samtidig finder et stort udvalg af brugt elektronik. Hvis du er i tvivl om, hvad der er galt med din telefon, så kan vores medarbejdere også hurtigt hjælpe dig. Det tager os som regel ikke lang tid at finde ud, hvad der skal gøres og komme med en eventuel pris.<br>
+        <a href="/butikker" class="underline y-link">FIND DIN NÆRMESTE GREENMIND AFDELING</a>
+      </p>
+      <h3 class="font-bold subtitle mb text-center">
+        GreenMind afdelinger
+      </h3>
+      <p class="text mb text-center">
+        Vi har værksteder i alle vores butikker.
+        Hvis vi ikke har travlt når du kommer, kan du få repareret din enhed mens du venter.
+      </p>
+      <p class="text mb text-center">
+        Danmarks mest erfarne<br>
+        Over 250.000 reparationer siden 2009<br>
+        Hurtig service<br>
+        Reparation på timebasis i alle butikker<br>
+        Kvalitet i højsædet<br>
+        Kun de bedste reservedele<br>
+        Overtagelse af garanti<br>
+        Vi overtager evt. rest-garanti på enhed
+      </p>
+      <h3 class="font-bold">
+        Overtagelse af garanti og reklamationsret
+      </h3>
+      <p class="text mb">
+        Når du får repareret din smartphone eller tablet hos GreenMind, så skal du ikke være nervøs for at miste garantien eller reklamationsretten på dit produkt. Vi overtager nemlig begge dele, når vi har haft din smartphone eller tablet inde til reparation. <br>
+        <a href="/handelsebetingelser" class="underline y-link">Læs GreenMinds hendelsbetingelser her.</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~/assets/css/infoPages.scss';
+</style>
+
+
