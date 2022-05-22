35eec7b121812cb93fb963fc2a89449dbc00da26
Jesper Witt <jw@bluecity.dk>
2022-05-22 19:19:11 +0000

    Update pages/WhyGreenmind.vue

--- pages/WhyGreenmind.vue
+++ pages/WhyGreenmind.vue
@@ -6,23 +6,23 @@
       </h1>
       <p class="text">
         Danskerne er rigtig glade for elektronik. Vi køber mere og mere. Men vi smider også mere og mere ud.<br>
-
+        <br>
         Vi bruger f.eks. i gennemsnit kun vores mobil i 18 mdr. og TV’et bliver allerede skiftet ud efter 2 år. Og selvom vi er rigtig gode til at skifte mobilen ud, gemmer vi næsten altid den gamle. For den er for god til at smide ud og for besværlig at sælge.<br>
-
-        Men det er ikke alle, der bare skal have den sidste nye Samsung Galaxy, Nokia Lumia eller iPhone. Hvis kvaliteten er i orden, er sidste års model helt perfekt.<br>
-
-        Derfor har vi startet GreenMind, der gør det nemt og sikkert at sælge og købe brugt elektronik.
+        <br>
+        Men det er ikke alle, der bare skal have den sidste nye Samsung Galaxy, OnePlus eller iPhone. Hvis kvaliteten er i orden, er sidste års model helt perfekt.<br>
+        <br>
+        Derfor er vores mission i GreenMind A/S at gøre det nemt og sikkert at sælge og købe brugt elektronik.
       </p>
       <p class="text mb bold">
         Det er sikkert at købe og sælge
       </p>
       <ul class="list-disc">
         <li class="text mb">
-          Uanset om du køber eller sælger, er det GreenMind, du handler med.
+          Uanset om du køber eller sælger, er det GreenMind A/S, du handler med.
         </li>
       </ul>
       <p class="text mb bold">
-        Du skal ikke videregive nogen former for oplysninger til privatpersoner.
+        Du skal ikke videregive nogen former for oplysninger til privatpersoner
       </p>
       <ul class="list-disc">
         <li class="text mb">
@@ -32,10 +32,10 @@
           Al elektronik bliver renset og nulstillet, og alt data slettes.
         </li>
         <li class="text mb">
-          Som køber har du 2 års garanti.
+          Som køber har du 3 års garanti.
         </li>
         <li class="text mb">
-          Hvis vi repararer din telefon eller computer så overtager vi evt. garanti og reklamationsret.
+          Hvis vi repararer din telefon, tablet eller computer så overtager vi evt. garanti og reklamationsret.
         </li>
       </ul>
       <p class="font-bold">
@@ -52,15 +52,13 @@
         Køb nyt med god samvittighed
       </p>
       <p class="text mb">
-        Når du f.eks. sælger din brugte mobil til GreenMind, forlænger du mobilens levetid.
-        Dette har en større miljømæssig betydning, end de fleste tror. Kun 15-20% af al forbrugerelektronik bliver genbrugt i dag.
+        Når du f.eks. sælger din brugte mobil til GreenMind A/S, forlænger du mobilens levetid. Dette har en større miljømæssig betydning, end de fleste tror. Kun 15-20% af al forbrugerelektronik bliver genbrugt i dag.
       </p>
       <p class="font-bold">
         Spar penge på din gode samvittighed
       </p>
       <p class="text mb">
-        Køber du elektronik i GreenMind, kan du spare op til tusindvis af kroner i forhold til at købe et nyt produkt,
-        som i nogle tilfælde kun er blevet ændret rent designmæssigt.
+        Køber du elektronik i GreenMind A/S, kan du spare op til tusindvis af kroner i forhold til at købe et nyt produkt, som i nogle tilfælde kun er blevet ændret rent designmæssigt.
       </p>
     </div>
   </div>
