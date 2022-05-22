be271af4359a8fc9ab75e5f94c376fa13cfda515
Jesper Witt <jw@bluecity.dk>
2022-05-22 17:50:47 +0000

    Update pages/DeaktiverFindMinIphone.vue

--- pages/DeaktiverFindMinIphone.vue
+++ pages/DeaktiverFindMinIphone.vue
@@ -10,8 +10,8 @@
       <iframe width="560" height="315" src="https://www.youtube.com/embed/u4DbFC8P11s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <h3 class="font-bold subtitle mb">Brug for hjælp?</h3>
       <p class="text mb">
-          Apple yder teknisk support, hvis du oplever problemer med at deaktivere Find min iPhone.
-
+          Apple yder teknisk support, hvis du oplever problemer med at deaktivere Find min iPhone.<br>
+          <br>
           Apple kan kontaktes på 80249625 eller via Apple Support.
       </p>
       <h3 class="font-bold subtitle mb">Trin 1, log ind</h3>
@@ -24,16 +24,16 @@
       </p>
       <h3 class="font-bold subtitle mb">Trin 3, vælg enheden</h3>
       <p class="text mb">
-          Klik på “Alle enheder” øverst i midten og vælg den rigtige enhed fra listen.
-
+          Klik på “Alle enheder” øverst i midten og vælg den rigtige enhed fra listen.<br>
+          <br>
           Sørg for at enheden er slukket. Cirklen til venstre for navnet på enheden skal være grå. Hvis cirklen er grøn, skal du først slukke enheden og derefter genindlæse siden, inden du går videre.
       </p>
       <h3 class="font-bold subtitle mb">Trin 4, fjern fra konto</h3>
       <p class="text mb">
-          Klik på “Alle enheder” øverst igen, klik på den rigtige enhed og klik derefter på det lille kryds til højre for navnet.
-
-          Alternativt kan der klikkes på “Fjern fra konto”. Det giver samme resultat.
-
+          Klik på “Alle enheder” øverst igen, klik på den rigtige enhed og klik derefter på det lille kryds til højre for navnet.<br>
+          <br>
+          Alternativt kan der klikkes på “Fjern fra konto”. Det giver samme resultat.<br>
+          <br>
           <bold>VIGTIGT:</bold> Du må IKKE slette enheden ved at klikke på “Slet iPhone”, “Slet Mac” eller “Slet iPad”. Ved sletning bliver Find min iPhone ikke deaktiveret. Det kan kun gøres ved at fjerne enheden fra kontoen som beskrevet ovenfor.
       </p>
       <h3 class="font-bold subtitle mb">Trin 5, bekræft valget</h3>
@@ -41,8 +41,8 @@
           Klik på “Fjern”.
       </p>
       <p class="text mb">
-          Apple yder teknisk support, hvis du oplever problemer med at deaktivere Find min iPhone.
-
+          Apple yder teknisk support, hvis du oplever problemer med at deaktivere Find min iPhone.<br>
+          <br>
           Apple kan kontaktes på 80249625 eller via Apple Support.
       </p>
     </div>
