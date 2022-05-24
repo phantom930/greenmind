4ef62bc9075d91e3f32ee62c3bc7d72cfd917d4b
Jesper Witt <jw@bluecity.dk>
2022-05-24 03:37:11 +0000

    Update pages/Cookies.vue

--- pages/Cookies.vue
+++ pages/Cookies.vue
@@ -7,6 +7,9 @@
       <p class="text mb">
         GreenMind.dk bruger cookies til at gemme oplysninger om brugerfunktioner såsom sorteringsmuligheder, hvor mange produkter, der vises og muligheden for at være logget ind på hjemmesiden som medlem. Vi bruger også cookies til at indsamle informationer om hvilke sider du besøger på hjemmesiden, så vi kan vise de annoncer, der er mest relevante for dig. Cookies bruges også til at analysere trafikken på hjemmesiden.<br>
         <br>
+        <h3 class="font-bold title mb">
+          Cookiebot informationer
+        </h3>
         Hvis du ikke ønsker at vi skal gemme cookies på din computer, har du mulighed for at blokere brugen af cookies ved at ændre en indstilling i din browser. Besøg da venligst din browsers hjemmeside for information og vejledning. Hvis du vælger at deaktivere cookies, vil dele af hjemmesiden ikke fungerer korrekt.
       </p>
       <br>
