8aab84ad2280b89b0aa8d3c473cc7d4026930af3
Jesper Witt <jw@bluecity.dk>
2022-05-24 01:24:41 +0000

    Update nuxt.config.ts, favicon.png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,8 +57,8 @@ export default {
     link: [
       {
         rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
+        type: 'image/png',
+        href: '/assets/images/favicon.png'
       }
     ]
   },
