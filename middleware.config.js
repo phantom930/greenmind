3c728eea39a25fe5635248b023469fcfc21ac43d
Leonardo Muniz <lmuniz@odoogap.com>
2022-05-26 08:24:23 -0300

    [ADD] custom extension api endpoint to handle refresh redirects

--- middleware.config.js
+++ middleware.config.js
@@ -1,4 +1,5 @@
 const odooBaseUrl = process.env.BACKEND_BASE_URL || process.env.BASE_URL || 'https://web-dev.greenmind.space/';
+const redirectsInvalidateToken = process.env.REDIRECT_INVALITION_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJncmVlbi1taW5kIiwibmFtZSI6Im9kb28iLCJpYXQiOjE1MTYyMzkwMjJ9.xCX61aKwPVveUAXFiH8BT2aS3gf_BmuJ9eJfcm5s9zE';
 const graphqlBaseUrl = `${odooBaseUrl}graphql/vsf`;
 const customQueries = require('./green-api/customQueries');
 const apis = require('./green-api/api');
@@ -9,13 +10,28 @@ module.exports = {
       location: '@vue-storefront/odoo-api/server',
       configuration: {
         odooBaseUrl,
-        graphqlBaseUrl
+        graphqlBaseUrl,
+        redirectsInvalidateToken
       },
       extensions: (extensions) => [
         ...extensions,
         {
           name: 'green-extension',
           extendApiMethods: apis
+        },
+        {
+          name: 'redirect-extension',
+          extendApp: ({ app, configuration }) => {
+            app.get('/redirects/invalidate/:token', (req, res) => {
+              const token = req.params.token;
+              if (token === configuration.redirectsInvalidateToken) {
+                return res.send('OK TOKEN');
+              }
+
+              return res.send('NOTHING');
+
+            });
+          }
         }
       ],
       customQueries
--- serverMiddleware/redirects.js
+++ serverMiddleware/redirects.js
@@ -1,15 +1,15 @@
-const redirects = require('./redirects.json')
+const redirects = require('./redirects.json');
 
 module.exports = function (req, res, next) {
-  var url = req.url.split('?')[0];
-  var urlParams = null;
-  if (req.url.includes("?")) {
-    urlParams = '?' + req.url.split('?')[1]
+  const url = req.url.split('?')[0];
+  let urlParams = null;
+  if (req.url.includes('?')) {
+    urlParams = '?' + req.url.split('?')[1];
   }
 
-  const redirect = redirects.find(r => r.from === url)
+  const redirect = redirects.find(r => r.from === url);
   if (redirect) {
-    var newLocation;
+    let newLocation;
     if (urlParams) {
       newLocation = redirect.to + urlParams;
     } else {
@@ -18,8 +18,8 @@ module.exports = function (req, res, next) {
     res.writeHead(301, {
       Location: newLocation
     });
-    res.end()
+    res.end();
   } else {
-    next()
+    next();
   }
-}
+};
