e9fd3a1e1972730e8f707fffb2c127615d26c081
Leonardo Muniz <lmuniz@odoogap.com>
2022-06-09 20:23:30 -0300

    [FIX] clean rebuild extension

--- helpers/rebuildExtension.js
+++ helpers/rebuildExtension.js
@@ -4,11 +4,10 @@ const consola = require('consola');
 const chalk = require('chalk');
 
 export default ({ app, configuration }) => {
-  app.get('/redirects/:token', (req, res) => {
+  app.get('/rebuild/:token', (req, res) => {
     const token = req.params.token;
 
     if (token === configuration.redirectsRefreshToken) {
-      const data = fsExtra.readFileSync('customRoutes/redirects.json');
 
       const buildProcess = child.exec('export NODE_ENV=production && yarn build --quiet');
 
@@ -20,7 +19,7 @@ export default ({ app, configuration }) => {
         child.exec('pm2 reload all');
       });
 
-      return res.send(data);
+      consola.info(chalk.bold('ODOO'), ' - App reloaded!');
     }
 
     return res.send('Wrong redirect token');
--- middleware.config.js
+++ middleware.config.js
@@ -3,8 +3,7 @@ const redirectsRefreshToken = process.env.REDIRECT_INVALITION_TOKEN || 'green-12
 const graphqlBaseUrl = `${odooBaseUrl}graphql/vsf`;
 const customQueries = require('./green-api/customQueries');
 const apis = require('./green-api/api');
-const redirectExtension = require('./helpers/redirectExtension');
-const redirectRefreshExtension = require('./helpers/redirectRefreshExtension');
+const rebuildExtension = require('./helpers/rebuildExtension');
 
 module.exports = {
   integrations: {
@@ -22,12 +21,8 @@ module.exports = {
           extendApiMethods: apis
         },
         {
-          name: 'redirect-extension',
-          extendApp: redirectExtension.default
-        },
-        {
-          name: 'redirect-refresh-extension',
-          extendApp: redirectRefreshExtension.default
+          name: 'rebuild-extension',
+          extendApp: rebuildExtension.default
         }
       ],
       customQueries
