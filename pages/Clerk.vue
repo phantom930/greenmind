3a02bfa6f5976a494e01cc6a76050403fee2eb03
Leonardo Muniz <lmuniz@odoogap.com>
2022-06-08 22:11:36 -0300

    [IMP] Clerk facet test

---
+++ pages/Clerk.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div>
+    <div id="clerk-search-filters" />
+
+    <span
+      class="clerk"
+      data-template="@ssearch"
+      data-query="MacBook"
+      data-facets-target="#clerk-search-filters"
+      data-facets-price-prepend="€"
+      data-facets-in-url="true"
+      data-facets-view-more-text="View More"
+      data-facets-searchbox-text="Search for "
+      data-facets-design="149603"
+    />
+  </div>
+</template>
+<script lang="ts">
+
+export default {
+};
+</script>
