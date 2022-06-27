ab36adf94b46347b1c1b177506948bf0a5bd4c06
Leonardo Muniz <lmuniz@odoogap.com>
2022-06-27 15:02:12 -0300

    [IMP] update logic of env error

--- ENV.ts
+++ ENV.ts
@@ -1,9 +1,15 @@
+const consola = require('consola');
+const chalk = require('chalk');
+
 const isDev = process.env.NODE_ENV !== 'production';
 
 const removeLastBar = (url) => url[url.length - 1] === '/' ? url.slice(0, -1) : url;
 
-const throwError = (name) => {
-  throw new Error(`Env ${name} must be set!`);
+const notDefinedEnvs = [];
+
+const pushToNotDefined = (name) => {
+  notDefinedEnvs.push(name);
+  return '';
 };
 
 /** Default values for DEV */
@@ -13,28 +19,35 @@ let gtagKey = 'G-YYZ9TG2MS2';
 let gtmKey = 'GTM-K9V7Q37';
 let klaviyoKey = 'NzYWAu';
 let nodeLocale = 'en-EN';
-let baseURL = 'https://web-dev.greenmind.space/';
+let baseURL = 'https://rune.greenmind.systems/';
 let siteUrl = 'http://localhost:3000';
-let odooBaseUrl = 'https://web-dev.greenmind.space/';
+let odooBaseUrl = 'https://rune.greenmind.systems/';
 let rebuildRefreshToken = '0ead60c3-d118-40be-9519-d531462ddc60';
 let baseDomain = 'vue-dev.greenmind.space';
 let sitemapCacheTime = 6000;
 
 /** ENV Vars for production */
 if (!isDev) {
-  clerkioKey = process.env.CLERK_PUBLIC_KEY || throwError('CLERK_PUBLIC_KEY');
-  cookieBotKey = process.env.COOKIEBOT_KEY || throwError('COOKIEBOT_KEY');
-  gtagKey = process.env.GTAG_KEY || throwError('GTAG_KEY');
-  gtmKey = process.env.GTM_KEY || throwError('GTM_KEY');
-  klaviyoKey = process.env.KLAVIYO_KEY || throwError('KLAVIYO_KEY');
-  nodeLocale = process.env.NODE_LOCALE || throwError('NODE_LOCALE');
-  // baseURL for images | differente when using CDN
-  baseURL = process.env.PUBLIC_PATH || process.env.BASE_URL || throwError('PUBLIC_PATH or BASE_URL');
-  siteUrl = removeLastBar(process.env.SITE_URL || throwError('SITE_URL'));
-  rebuildRefreshToken = process.env.INVALIDATION_KEY || throwError('INVALIDATION_KEY');
-  sitemapCacheTime = Number(process.env.SITEMAP_CACHE) || throwError('SITEMAP_CACHE');
-  odooBaseUrl = process.env.BACKEND_BASE_URL || throwError('BACKEND_BASE_URL');
-  baseDomain = process.env.BASE_DOMAIN || throwError('BASE_DOMAIN');
+  clerkioKey = process.env.CLERK_PUBLIC_KEY || pushToNotDefined('CLERK_PUBLIC_KEY');
+  cookieBotKey = process.env.COOKIEBOT_KEY || pushToNotDefined('COOKIEBOT_KEY');
+  gtagKey = process.env.GTAG_KEY || pushToNotDefined('GTAG_KEY');
+  gtmKey = process.env.GTM_KEY || pushToNotDefined('GTM_KEY');
+  klaviyoKey = process.env.KLAVIYO_KEY || pushToNotDefined('KLAVIYO_KEY');
+  nodeLocale = process.env.NODE_LOCALE || pushToNotDefined('NODE_LOCALE');
+  baseURL = process.env.PUBLIC_PATH || process.env.BASE_URL || pushToNotDefined('PUBLIC_PATH or BASE_URL');
+  siteUrl = removeLastBar(process.env.SITE_URL || pushToNotDefined('SITE_URL'));
+  rebuildRefreshToken = process.env.INVALIDATION_KEY || pushToNotDefined('INVALIDATION_KEY');
+  sitemapCacheTime = Number(process.env.SITEMAP_CACHE || pushToNotDefined('SITEMAP_CACHE'));
+  odooBaseUrl = process.env.BACKEND_BASE_URL || pushToNotDefined('BACKEND_BASE_URL');
+  baseDomain = process.env.BASE_DOMAIN || pushToNotDefined('BASE_DOMAIN');
+}
+
+if (notDefinedEnvs.length > 0) {
+  throw new Error(
+    `${chalk.red('Some env vars are not found!')}
+     ${chalk.greenBright('› ' + notDefinedEnvs.join(' \n › '))}
+     `
+  );
 }
 
 export {
