900dc6da7f0d536bb63b86ca8f32456fb881f2ab
Jesper Witt <jw@bluecity.dk>
2022-07-02 21:37:33 +0000

    Update pages/Recruitment.vue
    (cherry picked from commit c344d4d9b7461b7e947b58d01a42c1a16df74458)

--- pages/Recruitment.vue
+++ pages/Recruitment.vue
@@ -4,27 +4,26 @@
       <h1 class="font-bold title mb">
         Søg job hos GreenMind
       </h1>
+      <p class="text mb">
+        Hos GreenMind kæmper vi imod de mange millioner tons elektronik-skrald der produceres hvert år.
+        <br><br>
+        Med 14 fysisker butikker og over 200 ansatte er GreenMind Danmarks største forhandler af refurbished elektronik.
+        <br><br>
+        Ud over salg af refurbished elektronik, er GreenMind også blandt Danmarks største reparatørere af iPhones, Android Smartphones, Macbook og laptops.
+        <br><br>
+        Vi søger løbende både butikspersonale, teknikere m.m. Ser du ikke en stilling der lige passer på din profil herunder, så er du også meget velkommen til at sende os en uopfordret ansøgning.
+      </p>
+      <h3 class="font-bold subtitle mb">
+        Ledige stillinger hos GreenMind
+      </h3>
+      <div id="elvium_positions"></div>
+      <script>
+        new ElviumJobPostings('#elvium_positions', 'greenmind', 'da', {
+          domain: 'https://app.elvium.com',
+          referer_host: 'greenmind.dk'
+        })
+      </script>
     </div>
-    <p class="text mb">
-      Hos GreenMind kæmper vi imod de mange millioner tons elektronik-skrald der produceres hvert år.
-
-      Med 14 fysisker butikker og over 200 ansatte er GreenMind Danmarks største forhandler af refurbished elektronik.
-
-      Ud over salg af refurbished elektronik, er GreenMind også blandt Danmarks største reparatørere af iPhones, Android Smartphones, Macbook og laptops.
-
-      Vi søger løbende både butikspersonale, teknikere m.m. Ser du ikke en stilling der lige passer på din profil herunder, så er du også meget velkommen til at sende os en uopfordret ansøgning.
-    </p>
-    <br />
-    <h3 class="font-bold subtitle mb">
-      Ledige stillinger hos GreenMind
-    </h3>
-    <div id="elvium_positions"></div>
-    <script>
-      new ElviumJobPostings('#elvium_positions', 'greenmind', 'da', {
-        domain: 'https://app.elvium.com',
-        referer_host: 'https://greenmind.dk'
-      })
-    </script>
   </div>
 </template>
 
