0f88c36e8c470ba13049ea385accc0f3412bdd8a
Jesper Witt <jw@bluecity.dk>
2022-07-02 20:19:13 +0000

    Update pages/Recruitment.vue
    (cherry picked from commit 8ed21314f86025bb121155a39743112b2a7e63b6)

--- pages/Recruitment.vue
+++ pages/Recruitment.vue
@@ -6,20 +6,18 @@
       </h1>
     </div>
     <p class="text mb">
-        Vi tilbyder reparationer af både iPhones, iPads og MacBooks af alle varianter samt salg af brugte iPhones,
-        iPads og anden high-end elektronik. Vi har samlet set mere end 9 års erfaring med både salg og reparationer af
-        iPhones samt et brændende ønske om at blive Danmarks absolut bedste inden for vores fag.
-        GreenMind blev startet i 2009, under navnet, iRep, af Brian Funch. Kort efter fandt han sammen med Jakob Højland.
-        De havde begge en stor passion for teknologi, herunder særligt for Apples iPhones.
-        Desværre oplevede de ligesom mange andre, at skader var relativt udbredte på smartphones.
-        Det skyldes både den måde, de er designet på, men i ligeså høj grad, hvordan man bruger dem.
-        Fordi vores smartphones betyder så meget for vores hverdag, ønskede de at gøre det nemt og hurtigt at få dem repareret,
-        så man hurtigt kan komme tilbage til hverdagens mange andre gøremål.
-        Siden da er virksomheden vokset støt. Hos GreenMind arbejder vi hver dag på at være Danmarks bedste reparatør af smartphones og tablets.
-      </p>
-      <h3 class="font-bold subtitle mb">
-        Hvorfor skal du vælge GreenMind?
-      </h3>
+      Hos GreenMind kæmper vi imod de mange millioner tons elektronik-skrald der produceres hvert år.
+
+      Med 14 fysisker butikker og over 200 ansatte er GreenMind Danmarks største forhandler af refurbished elektronik.
+
+      Ud over salg af refurbished elektronik, er GreenMind også blandt Danmarks største reparatørere af iPhones, Android Smartphones, Macbook og laptops.
+
+      Vi søger løbende både butikspersonale, teknikere m.m. Ser du ikke en stilling der lige passer på din profil herunder, så er du også meget velkommen til at sende os en uopfordret ansøgning.
+    </p>
+    <br />
+    <h3 class="font-bold subtitle mb">
+      Ledige stillinger hos GreenMind
+    </h3>
     <div id="elvium_positions"></div>
     <script src="https://app.elvium.com/packs/js/export_job_postings.js"></script>
     <script>
