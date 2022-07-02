b53b4dd2e31cb52ba5a5d4776e5cbf84339c7258
Jesper Witt <jw@bluecity.dk>
2022-07-02 20:00:54 +0000

    Update pages/Recruitment.vue
    (cherry picked from commit b3401ec1433ca3dcf3011de543d6353951b29346)

---
+++ pages/Recruitment.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-wrap">
+    <div class="container">
+      <h1 class="font-bold title mb">
+        Om GreenMind
+      </h1>
+    </div>
+    <p class="text mb">
+        Vi tilbyder reparationer af både iPhones, iPads og MacBooks af alle varianter samt salg af brugte iPhones,
+        iPads og anden high-end elektronik. Vi har samlet set mere end 9 års erfaring med både salg og reparationer af
+        iPhones samt et brændende ønske om at blive Danmarks absolut bedste inden for vores fag.
+        GreenMind blev startet i 2009, under navnet, iRep, af Brian Funch. Kort efter fandt han sammen med Jakob Højland.
+        De havde begge en stor passion for teknologi, herunder særligt for Apples iPhones.
+        Desværre oplevede de ligesom mange andre, at skader var relativt udbredte på smartphones.
+        Det skyldes både den måde, de er designet på, men i ligeså høj grad, hvordan man bruger dem.
+        Fordi vores smartphones betyder så meget for vores hverdag, ønskede de at gøre det nemt og hurtigt at få dem repareret,
+        så man hurtigt kan komme tilbage til hverdagens mange andre gøremål.
+        Siden da er virksomheden vokset støt. Hos GreenMind arbejder vi hver dag på at være Danmarks bedste reparatør af smartphones og tablets.
+      </p>
+      <h3 class="font-bold subtitle mb">
+        Hvorfor skal du vælge GreenMind?
+      </h3>
+    <div id="elvium_positions"></div>
+    <script src="https://app.elvium.com/packs/js/export_job_postings.js"></script>
+    <script>
+      new ElviumJobPostings('#elvium_positions', 'greenmind', 'da', {
+        domain: 'https://app.elvium.com',
+        referer_host: 'https://greenmind.dk'
+      })
+    </script>
+  </div>
+</template>
