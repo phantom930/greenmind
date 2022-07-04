d67c8cf1fd58f0521a3745b98aad5316eee9cce6
Jesper Witt <jw@bluecity.dk>
2022-07-04 11:02:04 +0000

    Update pages/Recruitment.vue

--- pages/Recruitment.vue
+++ pages/Recruitment.vue
@@ -7,11 +7,11 @@
       <p class="text mb">
         Hos GreenMind kæmper vi imod de mange millioner tons elektronik-skrald der produceres hvert år.
         <br><br>
-        Med 14 fysisker butikker og over 200 ansatte er GreenMind Danmarks største forhandler af refurbished elektronik.
+        Med 15 fysiske butikker, online og over 200 ansatte er GreenMind blandt Danmarks største reparatører af iPhones, Android Smartphones, MacBook og lap tops, ligesom vi også køber og sælger elektronik med en grøn agenda.
         <br><br>
-        Ud over salg af refurbished elektronik, er GreenMind også blandt Danmarks største reparatørere af iPhones, Android Smartphones, Macbook og laptops.
+        Vi søger løbende både butikspersonale, teknikere m.m. Har du lyst til at være med til at gøre en forskel OG få de bedste kolleger, så se de ledige stillinger nedenfor.
         <br><br>
-        Vi søger løbende både butikspersonale, teknikere m.m. Ser du ikke en stilling der lige passer på din profil herunder, så er du også meget velkommen til at sende os en uopfordret ansøgning.
+        Ser du ikke en stilling der lige passer på din profil herunder, så er du også meget velkommen til at sende os en uopfordret ansøgning.
       </p>
       <h3 class="font-bold subtitle mb">
         Ledige stillinger hos GreenMind
