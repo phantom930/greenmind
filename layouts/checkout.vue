69c999aa1b80c4a349e3914010481f8eb2264d9b
Leonardo Muniz <lmuniz@odoogap.com>
2022-07-09 17:35:45 -0300

    [FIX] cart null from server to client

--- layouts/checkout.vue
+++ layouts/checkout.vue
@@ -18,6 +18,7 @@ import LazyHydrate from 'vue-lazy-hydration';
 import Notification from '~/components/Notification';
 import { onSSR } from '@vue-storefront/core';
 import { useCart } from '@vue-storefront/odoo';
+import { onMounted } from '@nuxtjs/composition-api';
 
 export default {
   name: 'DefaultLayout',
@@ -28,7 +29,15 @@ export default {
   setup (props, context) {
     const { setCart } = useCart();
 
-    setCart(context.root.context.$vsf.$odoo.cart);
+    onSSR(async () => {
+      setCart(context.root.context.$vsf.$odoo.cart);
+    });
+
+    onMounted(() => {
+      setCart(null);
+      const { data } = await context.root.context.$vsf.$odoo.api.cartLoadWithUpdate({ updatePrices: true });
+      setCart(data.cart);
+    });
   }
 };
 </script>
