cc546f26c5a4447cf396c8756f25d9351d85bcac
Jesper Witt <jw@bluecity.dk>
2022-07-13 10:07:21 +0000

    Update pages/DiscountCampaigns.vue

--- pages/DiscountCampaigns.vue
+++ pages/DiscountCampaigns.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="text-wrap">
-     <span class="clerk" data-template="@discount-campaign-page"></span>
+  <div
+    id="clerk-content"
+    data-template="@discount-campaign-page">
   </div>
 </template>
+
+<script type="text/javascript">
+  // initialize Clerk.io content via JavaScript.
+  Clerk('content', '#clerk-content');
+</script>
 
 <style lang="scss" scoped>
 @import '~/assets/css/infoPages.scss';
