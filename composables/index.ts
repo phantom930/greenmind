ecd31598c7fa0a738ec8d0c0ba2a2267a5204d2a
Leonardo Muniz <lmuniz@odoogap.com>
2022-07-25 18:15:47 -0300

    [ADD] adyen internal composable

--- composables/index.ts
+++ composables/index.ts
@@ -12,12 +12,14 @@ import usePayment from './usePayment';
 import useGiftCard from './useGiftCard';
 import useCoupon from './useCoupon';
 import useStore from './useStore';
+import useAdyenDropInPayment from './useAdyenDropInPayment';
 import useGetLocale from './useGetLocale';
 import cartGetters from './getters/cartGetters';
 import productGetters from './getters/productGetters';
 import facetGetters from './getters/facetGetters';
 
 export {
+  useAdyenDropInPayment,
   useGiftCard,
   usePayment,
   useUiHelpers,
